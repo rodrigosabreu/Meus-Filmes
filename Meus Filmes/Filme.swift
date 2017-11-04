62c1e29ca25b18bfadf378dc5d442fed1b138c86
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-04 09:53:34 -0200

    Array de Objetos - Filmes

---
+++ Meus Filmes/Filme.swift
@@ -0,0 +1,25 @@
+//
+//  Filme.swift
+//  Meus Filmes
+//
+//  Created by Rodrigo Abreu on 04/11/17.
+//  Copyright © 2017 Rodrigo Abreu. All rights reserved.
+//
+
+import UIKit
+
+class Filme{
+    
+    var titulo: String!
+    var descricao: String!
+    var imagem: UIImage!
+    
+    init(titulo: String, descricao: String){
+        self.titulo = titulo
+        self.descricao = descricao
+        
+    }
+    
+    
+    
+}
--- Meus Filmes/ViewController.swift
+++ Meus Filmes/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var filmes: [Filme] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var filme: Filme
+        
+        filme = Filme(titulo: "Filme 1", descricao: "descricao 1")
+        filmes.append( filme )
+        
+        filme = Filme(titulo: "Filme 2", descricao: "descricao 2")
+        filmes.append( filme )
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filmes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let filme: Filme = filmes[ indexPath.row ]
+        let celulaReuso = "celulaReuso"
+        
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath)
+        celula.textLabel?.text = filme.titulo
+        
+        return celula
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
